b287584a6c3bc3d6ca1f0fc5122f3a0d4c448c40
drseilzug <dr.seilzug@gmail.com>
2017-09-06 16:02:18 +0200

    fixes and __str__

--- GoGote/GoGote.py
+++ GoGote/GoGote.py
@@ -19,15 +19,34 @@ class Board:
         self.caps_black = caps_black
         self.caps_white = caps_white
         self.move_number = move_number
+        self.size = size
+
+    def __str__(self):
+        """String representation of the board"""
+        repr = ""
+        for line in self.postion:
+            for col in line:
+                if col == white:
+                    repr += "W"
+                elif col == black:
+                    repr += "B"
+                else:
+                    repr += "."
+            repr += "\n"
 
     def get_neighbors(self, x, y):
         """
         Returns a set with the coords of all neighbours of (x,y)
+        as tupels
         """
         neighbours = set()
         for (x, y) in [(x+1, y), (x-1, y), (x, y-1), (x, y+1)]:
-            if x <= self.size and y <= self.size:
+            if 0 <= x < self.size and 0 <= y < self.size:
                 neighbours.add((x, y))
         return neighbours
 
+
 # Testing area
+testspiel = Board()
+print(testspiel.get_neighbors(1, 1))
+print(testspiel.get_neighbors(3, 5))
