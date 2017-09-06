a8abd00e714917bb4900ca755d12af4e385d6bb3
drseilzug <dr.seilzug@gmail.com>
2017-09-06 18:04:52 +0200

    fixes added set_field method

--- GoGote/GoGote.py
+++ GoGote/GoGote.py
@@ -11,6 +11,7 @@ class Board:
     empty = 0
     black = 1
     white = 2
+    ko = 3
 
     def __init__(self, move_number=0, player=black, size=19,
                  caps_black=0, caps_white=0):
@@ -22,17 +23,23 @@ class Board:
         self.size = size
 
     def __str__(self):
-        """String representation of the board"""
+        """String representation of the board
+
+        TODO: represent last move (maybe italic or smth)
+        """
         repr = ""
         for line in self.postion:
             for col in line:
-                if col == white:
-                    repr += "W"
-                elif col == black:
-                    repr += "B"
+                if col == self.white:
+                    repr += "O"
+                elif col == self.black:
+                    repr += "#"
+                elif col == self.ko:
+                    repr += "*"
                 else:
                     repr += "."
             repr += "\n"
+        return repr
 
     def get_neighbors(self, x, y):
         """
@@ -45,8 +52,45 @@ class Board:
                 neighbours.add((x, y))
         return neighbours
 
+    def set_field(self, x, y, status):
+        """
+        sets field (x, y) to status.
+
+        legal inputs for status are:
+            empty: 0, "e", "empty", self.empty
+            black: 1, "b", "black", self.black
+            white: 2, "w", "white", self.white
+            ko:    3, "k", "ko",    self.ko
+        """
+        if status in (0, "e", "empty"):
+            self.postion[x][y] = self.empty
+        elif status in (1, "b", "black"):
+            self.postion[x][y] = self.black
+        elif status in (2, "w", "white"):
+            self.postion[x][y] = self.white
+        elif status in (3, "k", "ko"):
+            self.postion[x][y] = self.ko
+        else:
+            print("wrong input")  # TODO ERROR handeling
+
+        def get_group_info(self, x, y):
+            """
+            returns information on the group at (x, y) as a dictonary
+                "libs":boolean True if group has liberties
+                "stones":set contains coordiantes of stones belonging to group
+            """
+            pass  # TODO implement functionality
+
 
 # Testing area
-testspiel = Board()
+testspiel = Board(size=9)
 print(testspiel.get_neighbors(1, 1))
 print(testspiel.get_neighbors(3, 5))
+
+testspiel.set_field(2, 4, testspiel.black)
+testspiel.postion[1][4] = 1
+testspiel.postion[3][5] = 1
+testspiel.postion[3][4] = 1
+testspiel.postion[6][6] = 2
+testspiel.postion[7][6] = 2
+print(testspiel)
