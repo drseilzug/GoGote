04698e35f5fbaefdde6dd22c243ba71be6582c35
drseilzug <dr.seilzug@gmail.com>
2017-09-07 14:25:19 +0200

    added a bunch of methodes. core class functionality is now implemented

--- GoGote/GoGote.py
+++ GoGote/GoGote.py
@@ -73,19 +73,67 @@ class Board:
         else:
             print("wrong input")  # TODO ERROR handeling
 
-        def get_group_info(self, x, y):
-            """
-            returns information on the group at (x, y) as a dictonary
-                "libs":boolean True if group has liberties
-                "stones":set contains coordiantes of stones belonging to group
-            """
-            pass  # TODO implement functionality
+    def is_empty(self, x, y):
+        """
+        checks if (x, y) has no stones on it
+        """
+        if self.postion[x][y] in [self.empty, self.ko]:
+            return True
+        else:
+            return False
+
+    def is_friend(self, x, y, x2, y2):
+        """
+        Checks whether (x, y) and (x2, y2) are of the same
+        faction.
+        """
+        status1 = self.postion[x][y]
+        status2 = self.postion[x2][y2]
+        # empy fields shall be considered friedly to each other, incl ko
+        for status in [status1, status2]:
+            if status == self.ko:
+                status = self.empty
+        if status1 == status2:
+            return True
+        else:
+            return False
+
+    def get_group_info(self, x, y):
+        """
+        returns information on the group at (x, y) as a dictonary
+            "libs":boolean True if group has liberties
+            "stones":set contains coordiantes of stones belonging to group
+                    only stones that were ckecked until liberty was found.
+                    i.e. if libs == False stones contains whole group
+        """
+        to_check = {(x, y)}
+        checked = set()
+        libs = False
+        while len(to_check) > 0:
+            current_stone = to_check.pop()
+            neighbours = self.neighbours(*current_stone) - checked
+            for stone in neighbours:
+                if self.is_friend(*current_stone, *stone):
+                    to_check.update(stone)
+                elif self.is_empty:
+                    libs = True
+                    break
+            checked.update(current_stone)
+        return {'libs': libs, 'group': checked}
+
+    def kill_stone(self, x, y):
+        if self.postion[x][y] == self.black:
+            self.caps_black += 1
+            self.set_field = self.empty
+        elif self.postion[x][y] == self.white:
+            self.caps_white += 1
+            self.set_field = self.empty
+        else:
+            print("cant kill nonexisting stone")  # TODO: ERROR handeling
 
 
 # Testing area
 testspiel = Board(size=9)
-print(testspiel.get_neighbors(1, 1))
-print(testspiel.get_neighbors(3, 5))
 
 testspiel.set_field(2, 4, testspiel.black)
 testspiel.postion[1][4] = 1
