7a8f8415d504b369df90c921dd184805491d26de
drseilzug <dr.seilzug@gmail.com>
2017-09-18 01:42:24 +0200

    added TODO list for methods in Game Class

--- GoGote/Game.py
+++ GoGote/Game.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python3
 import Board
+import Player
 
 
 class Game:
     """
     here be dragons
     """
+
+    def __init__(self):
+        pass
+
+    # Methods to implement
+    # def make_move(self, x, y):
+    # def pass(self):
+    # def check_ko(self):
