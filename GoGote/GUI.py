1c490523307944aebfcb760dec21f7d8509508c5
drseilzug <dr.seilzug@gmail.com>
2017-09-23 20:33:03 +0200

    Layout, code structured a little passing now works (kind of)

--- GoGote/GUI.py
+++ GoGote/GUI.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 import sys
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QAction, qApp,
-                             QWidget)
+                             QWidget, QPushButton, QHBoxLayout, QVBoxLayout,
+                             QLabel)
 from PyQt5.QtGui import QIcon, QPainter, QPen
-from PyQt5.QtCore import QPoint
-from PyQt5 import Qt
+from PyQt5.QtCore import QPoint  # , pyqtSignal
+# from PyQt5 import Qt
 
 from Game import Game
 
@@ -18,18 +19,19 @@ class MainWin(QMainWindow):
 
     def initUI(self):
         # statusbar
-        self.statusbar = self.statusBar()
-        self.statusbar.showMessage('Ready')
+        self.statusB = self.statusBar()
+        self.statusB.showMessage('Ready')
 
         # setup window
         self.setGeometry(50, 50, 800, 600)
         self.setWindowTitle('GoGote')
         self.setWindowIcon(QIcon('png/icon.png'))
 
-        # Initialize a Game and Board_GUI
-        game = Game()
-        theBoard = Board_GUI(self, game.currentBoard)
-        self.setCentralWidget(theBoard)
+        # Initialize central Game Widget
+        newGame = Game()
+        gameW = GameWidget(self, newGame)
+        self.setCentralWidget(gameW)
+        # self.statusB.showMessage(self.statusText())
 
         # define Action to quit programm
         exitAct = QAction('&Exit', self)
@@ -57,10 +59,86 @@ class MainWin(QMainWindow):
         """
         # TODO implement coordinates. atm shows/hides statusbar for testing
         if state:
-            self.statusbar.show()
-            self.statusbar.showMessage('Ready')
+            self.statusB.show()
         else:
-            self.statusbar.hide()
+            self.statusB.hide()
+
+    def statusText(self):
+        """ generates statusbar Text"""
+        status = ""
+        status += "Move: " + str(self.game.moveCounter) + ". "
+        playerStr = ""
+        if self.board.player == self.board.black:
+            playerStr = "Black"
+        elif self.board.player == self.board.white:
+            playerStr = "White"
+        status += playerStr + " to play."
+        return status
+
+
+class GameWidget(QWidget):
+    """
+    A Widget that contains everything for a game
+    Game is supposed the be a child of a QMainWindow
+    """
+    def __init__(self, parent, game):
+        super().__init__()
+        self.initGame(game)
+
+    def initGame(self, game):
+        self.game = game
+        self.board = self.game.currentBoard
+
+        # create child Widgets
+        boardW = Board_GUI(self, self.board)
+        controleW = Controle_GUI(self, self.game)
+        infoW = InfoWidget(self, self.game)
+
+        # create Layout
+        hbox = QHBoxLayout()
+        hbox.addWidget(boardW, stretch=1)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(infoW)
+        vbox.addWidget(controleW, stretch=1)
+        hbox.addLayout(vbox)
+
+        self.setLayout(hbox)
+
+
+class InfoWidget(QWidget):
+    """ Widget desplays information on the game"""
+    def __init__(self, parent, game):
+        super().__init__()
+        self.initUI(game)
+
+    def initUI(self, game):
+        testlabel = QLabel()
+        testlabel.setText("Placeholder")
+
+        # Layout
+        hbox = QHBoxLayout()
+        hbox.addWidget(testlabel)
+        self.setLayout(hbox)
+
+
+class Controle_GUI(QWidget):
+    """contains the controle elements for the game"""
+    # msg2StatusB = pyqtSignal(str)
+    def __init__(self, parent, game):
+        super().__init__()
+        self.initControle(game)
+
+    def initControle(self, game):
+        self.game = game
+
+        passBtn = QPushButton('Pass')
+        passBtn.clicked.connect(self.game.passMove)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(passBtn)
+
+        self.setLayout(vbox)
 
 
 class Board_GUI(QWidget):
@@ -76,7 +154,8 @@ class Board_GUI(QWidget):
         # moustracking to highlight position
         self.setMouseTracking(True)
         self.showCoords = False  # TODO
-
+        self.update()
+        self.update()
         # TESTING TESTING TESTING:
 
     def boardWidth(self):
@@ -116,12 +195,15 @@ class Board_GUI(QWidget):
         for (pointT, pointB) in zip(self.grid[0], self.grid[-1]):
             painter.drawLine(pointT, pointB)
         self.drawHoshis(painter)
+        # Test TODO
+        self.drawStone(painter, (6, 6), self.board.black)
 
     def drawHoshis(self, painter):
         """ Draws Hoshi dots"""
         hoshis = []
         pen = QPen()
         pen.setWidth(8)
+        pen.setCapStyle(0x20)
         painter.setPen(pen)
 
         for (x, y) in self.board.getHoshis():
@@ -130,7 +212,7 @@ class Board_GUI(QWidget):
             painter.drawPoint(point)
 
     def drawPosition(self, painter):
-        """draws the positionon the Grid"""
+        """draws the positionon onto the Grid"""
         pass  # TODO
 
     def drawStone(self, painter, positionon, color):
