da4dcad19bc41c46e825ee2b6d5550efd565d1e1
drseilzug <dr.seilzug@gmail.com>
2017-09-24 22:21:10 +0200

    introduced some Class constants for sizing certain things

--- GoGote/GUI.py
+++ GoGote/GUI.py
@@ -3,8 +3,8 @@ import sys
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QAction, qApp,
                              QWidget, QPushButton, QHBoxLayout, QVBoxLayout,
                              QLabel)
-from PyQt5.QtGui import QIcon, QPainter, QPen
-from PyQt5.QtCore import QPoint  # , pyqtSignal
+from PyQt5.QtGui import QIcon, QPainter, QPen, QColor
+from PyQt5.QtCore import Qt, QPointF  # , pyqtSignal
 # from PyQt5 import Qt
 
 from Game import Game
@@ -91,7 +91,7 @@ class GameWidget(QWidget):
 
         # create child Widgets
         boardW = Board_GUI(self, self.board)
-        controleW = Controle_GUI(self, self.game)
+        controleW = ControleWidget(self, self.game)
         infoW = InfoWidget(self, self.game)
 
         # create Layout
@@ -104,6 +104,12 @@ class GameWidget(QWidget):
         hbox.addLayout(vbox)
 
         self.setLayout(hbox)
+        self.game.playMove(1, 0)
+        self.game.playMove(0, 0)
+        self.game.playMove(0, 1)
+        self.game.playMove(1, 1)
+        # self.update()
+        print("test")
 
 
 class InfoWidget(QWidget):
@@ -122,7 +128,7 @@ class InfoWidget(QWidget):
         self.setLayout(hbox)
 
 
-class Controle_GUI(QWidget):
+class ControleWidget(QWidget):
     """contains the controle elements for the game"""
     # msg2StatusB = pyqtSignal(str)
     def __init__(self, parent, game):
@@ -143,43 +149,51 @@ class Controle_GUI(QWidget):
 
 class Board_GUI(QWidget):
     """cointain the grafical representation of the Board"""
+
+    # ratio of boardersize compared to the size of one base square
+    borderRatio = 0.8
+    baseRectRatio = 14/15  # 12/13 for normal ratio but looks weird
+    stoneScale = 0.467
+
     def __init__(self, parent, board):
         super().__init__()
 
-        self.initBoard(board)
+        self.initUI(board)
 
-    def initBoard(self, board):
+    def initUI(self, board):
         self.board = board
         self.grid = []
         # moustracking to highlight position
         self.setMouseTracking(True)
         self.showCoords = False  # TODO
-        self.update()
-        self.update()
-        # TESTING TESTING TESTING:
 
     def boardWidth(self):
         """returns the max width fitting into widget"""
         width = self.contentsRect().width()
         height = self.contentsRect().height()
 
-        return min(width, height*12/13)
+        return min(width, height*self.baseRectRatio)
 
     def boardHeight(self):
         """returns the max width fitting into widget """
-        return self.boardWidth()*(13/12)
+        return self.boardWidth()*(1/self.baseRectRatio)
 
     def makeGrid(self):
         """returns coords for the Grid according to current window mesures"""
         # 0.8*baserectangle size for each border
-        denom = self.board.size + 1.6
+        denom = self.board.size + 2*self.borderRatio
         baseWidth = self.boardWidth() / denom
         baseHeight = self.boardHeight() / denom
 
-        partionWidth = [(0.8+x)*baseWidth for x in range(self.board.size)]
-        partionHeight = [(0.8+x)*baseHeight for x in range(self.board.size)]
+        leftOffset = (self.contentsRect().width()-self.boardWidth())/2
+        topOffset = (self.contentsRect().height()-self.boardHeight())/2
 
-        grid = [[QPoint(x, y) for x in partionWidth] for y in partionHeight]
+        partionWidth = [leftOffset+(self.borderRatio+x)*baseWidth
+                        for x in range(self.board.size)]
+        partionHeight = [topOffset+(self.borderRatio+x)*baseHeight
+                         for x in range(self.board.size)]
+
+        grid = [[QPointF(x, y) for x in partionWidth] for y in partionHeight]
         self.grid = grid
 
     def paintEvent(self, event):
@@ -195,28 +209,44 @@ class Board_GUI(QWidget):
         for (pointT, pointB) in zip(self.grid[0], self.grid[-1]):
             painter.drawLine(pointT, pointB)
         self.drawHoshis(painter)
-        # Test TODO
-        self.drawStone(painter, (6, 6), self.board.black)
+        self.drawPosition(painter)
 
     def drawHoshis(self, painter):
         """ Draws Hoshi dots"""
         hoshis = []
         pen = QPen()
         pen.setWidth(8)
-        pen.setCapStyle(0x20)
+        pen.setCapStyle(Qt.RoundCap)
         painter.setPen(pen)
 
         for (x, y) in self.board.getHoshis():
             hoshis.append(self.grid[x][y])
         for point in hoshis:
             painter.drawPoint(point)
+        painter.setPen(QPen())
 
     def drawPosition(self, painter):
         """draws the positionon onto the Grid"""
-        pass  # TODO
+        for x in range(self.board.size):
+            for y in range(self.board.size):
+                self.drawStone(painter, self.grid[x][y],
+                               self.board.postion[x][y])
 
-    def drawStone(self, painter, positionon, color):
+    def drawStone(self, painter, targetPoint, color):
         """draws a Stone of color at position"""
+        unit = (self.grid[0][0]-self.grid[0][1]).manhattanLength()
+        radius = unit * self.stoneScale
+        if color == self.board.black:
+            stoneColor = QColor(255, 255, 255)
+        elif color == self.board.white:
+            stoneColor = QColor(0, 0, 0)
+        else:
+            return
+        painter.setBrush(stoneColor)
+        painter.drawEllipse(targetPoint, radius, radius)
+
+    def drawLastMove(self, painter):
+        """draws a square on the last placed stone"""
         pass  # TODO
 
 
